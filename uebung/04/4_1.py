8063ef1af13789c6d11620cfb21c420cbf23865e
nld <nick_daiber@riseup.net>
2025-12-03 17:17:54 +0100

    4.3

--- uebung/04/4_1.py
+++ uebung/04/4_1.py
@@ -7,12 +7,14 @@ for i in range(1, 11):
     pi = (i - 1/2) / n
     percentiles[i] = scipy.stats.norm(loc=0, scale=1).ppf(pi)
 percentiles[n + 1] = percentiles[n] + 0.5
+print(percentiles)
 
-f_str = f"x,y\n {percentiles[1] - 0.5},0\n{percentiles[1]},0\n\n"
+f_str = f"x,y\n -4,0\n{percentiles[1]},0"
 marks_str = "x,y\n"
 for x in range(1, n + 1):
-    f_str += f"{percentiles[x]},{x / 10}\n{percentiles[x+1]},{x/10}\n\n"
+    f_str += f"\n\n{percentiles[x]},{x / 10}\n{percentiles[x+1]},{x/10}"
     marks_str += f"{percentiles[x]},{x / 10}\n"
+f_str += "\n4,1"
 with open("4_1X.csv", "w") as f:
     f.write(f_str)
 with open("4_1Xm.csv", "w") as f:
